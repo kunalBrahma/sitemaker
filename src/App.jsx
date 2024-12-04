c5baa601b71679aa314b0a84fbcb58e4799434bf
Wiz Script <80410452+kunalBrahma@users.noreply.github.com>
2024-12-04 17:17:17 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import About from "./components/About"
 import Cursor from "./components/Cursor"
 import Hero from "./components/Hero"
-import Marquee from "./components/Marquee"
 import Navbar from "./components/Navbar"
 import Services from "./components/Services"
 
@@ -14,7 +13,7 @@ const App = () => {
       <Hero/>
       <Services/>
       <About/>
-      <Marquee/>
+
       <section className="h-dvh w-screen"></section>
     </main>
   )
